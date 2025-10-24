262943850e555188fb23a784d283efe18895ea73
Aakarsh Kumar <162731300+aakarsh890@users.noreply.github.com>
2025-10-24 16:22:46 +0530

    Update api.js

--- my-react-app/src/api.js
+++ my-react-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // change if backend runs elsewhere
+  baseURL: "https://xml-file-website-backend.onrender.com", // change if backend runs elsewhere
 });
 
 // Upload XML
